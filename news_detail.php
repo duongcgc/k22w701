0a0cd9d8b5e7cba292706a1d07ea88bec03659c9
John Nguyen <duongcgc@gmail.com>
2022-12-06 19:44:05 +0700

    update news detail + news detail + data

---
+++ news_detail.php
@@ -0,0 +1,7 @@
+<?php include 'templates/config.php'; ?>
+
+<?php include 'templates/header.php'; ?>
+
+<?php include 'templates/get-data-news-detail.php'; ?>
+
+<?php include 'templates/footer.php'; ?>
--- templates/get-data-news-detail.php
+++ templates/get-data-news-detail.php
@@ -1,7 +1,11 @@
 <?php 
 
+
+// Lấy giá trị id hiện tại ở trên url
+$current_id = isset($_GET['id']) ? $_GET['id'] : 0;
+
 // tạo lệnh đọc dữ liệu
-$sql = "SELECT * FROM `tintuc`";
+$sql = "SELECT * FROM `tintuc` WHERE `news_id` = $current_id" ;
 
 // thực hiện lệnh đã tạo
 $news_data = $conn_w701->prepare($sql);
@@ -13,14 +17,15 @@ $news_item = $news_data->fetchAll(PDO::FETCH_ASSOC);
 foreach($news_item as $item) {    
     
     ?>
+        <p><a href="news_main.php">Trang chủ</a></p>
         <div class="news_item" style="overflow: auto;">
             <div class="news_item__thumbnail" style="float: left; margin-right:15px;">
-                <a href="news_detail.php?id=<?php echo $item['news_id']; ?>"><img src="<?php echo $item['hinhanh']; ?>" alt="" style="width:100px; height:auto;border:solid 2px gray;"></a>
+                <a href=""><img src="<?php echo $item['hinhanh']; ?>" alt="" style="width:100px; height:auto;border:solid 2px gray;"></a>
             </div>
             <div class="news_item__summary" style="float: ;left">
-                <h2 class="news_item__heading"><a href="news_detail.php?id=<?php echo $item['news_id']; ?>"><?php echo $item['tieude']; ?></a></h2>        
+                <h2 class="news_item__heading"><a href=""><?php echo $item['tieude']; ?></a></h2>        
                 <p><span class="news_item__date"><?php echo $item['ngaydang']; ?></span></p>
-                <div class="readmore"><a href="news_detail.php?id=<?php echo $item['news_id']; ?>">Xem chi tiết ...</a></div>
+                <div class="news_item__content"><?php echo $item['noidung']; ?></div>
             </div>
         </div>
         <br>
